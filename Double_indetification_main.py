bcbdccc674e088f3c970740bc888453dc8f17bbb
Artem Zaglubotskii <mr_a953@mail.ru>
2019-01-25 19:50:42 +0300

    Add files via upload

---
+++ Double_indetification_main.py
@@ -0,0 +1,40 @@
+from py_ecc import secp256k1
+from web3 import Web3
+web3=Web3(Web3.HTTPProvider('https://sokol.poa.network'))
+
+uuid="e40989524e3f46a4894d4460fac60251"
+pin_code="3735"
+
+def generate_private_key(uuid,pin_code):
+    privateKey=web3.sha3(web3.sha3(web3.sha3(web3.sha3(web3.sha3(text='')+int(uuid,16).to_bytes(16, 'big')+int(pin_code[0],16).to_bytes(1, 'big'))+int(uuid,16).to_bytes(16, 'big')+int(pin_code[1],16).to_bytes(1, 'big'))+int(uuid,16).to_bytes(16, 'big')+int(pin_code[2],16).to_bytes(1, 'big'))+int(uuid,16).to_bytes(16, 'big')+int(pin_code[3],16).to_bytes(1, 'big')).hex()
+    return privateKey
+
+def privToPub(priv):
+     priv = int(priv, 16).to_bytes(32, 'big')
+     res = secp256k1.privtopub(priv)
+     x = res[0].to_bytes(32, 'big')
+     y = res[1].to_bytes(32, 'big')
+     return x+y
+
+def pubToAddr(pub):
+     return web3.sha3(pub).hex()[24:]
+
+def privToAddr(priv):
+    return pubToAddr(privToPub(priv))
+
+print(web3.sha3(text='')+int(uuid, 16).to_bytes(16, 'big')+int(pin_code[0], 16).to_bytes(1, 'big'))
+private=generate_private_key(uuid,pin_code)
+#private = '2cdc6fc375180afbdd78e7541b4eacb03faa70d0d78de162b1625cdef855540d'
+print(private)
+public=privToPub(private)
+print(public)
+addr=pubToAddr(public)
+addrlast="0x"+addr[len(addr)-40:]
+print(addrlast)
+
+
+def getBalance(addr):
+    balance=web3.eth.getBalance(Web3.toChecksumAddress(addr))
+    return balance
+balance=getBalance(addrlast)
+print(balance)
---
+++ HeadPose.py
@@ -0,0 +1,62 @@
+import cognitive_face as CF
+import os
+
+KEY = '189cbe2ce40044a7bd06c149fc00e67b'  # Replace with a valid Subscription Key here.
+CF.Key.set(KEY)
+
+BASE_URL = 'https://westcentralus.api.cognitive.microsoft.com/face/v1.0'  # Replace with your regional Base URL
+CF.BaseUrl.set(BASE_URL)
+
+position = input()
+
+
+def turn_left(resultp, i):
+    result2 = resultp[0]['faceAttributes']['headPose']['yaw']
+    if (result2 > 15.0):
+        return str(i)
+    else:
+        return ''
+
+
+def turn_right(resultp, i):
+    result2 = resultp[0]['faceAttributes']['headPose']['yaw']
+    if (result2 < -15.0):
+        return str(i)
+    else:
+        return ''
+
+
+def roll_left(resultp, i):
+    result2 = resultp[0]['faceAttributes']['headPose']['roll']
+    if (result2 > 15.0):
+        return str(i)
+    else:
+        return ''
+
+
+def roll_right(resultp, i):
+    result2 = resultp[0]['faceAttributes']['headPose']['roll']
+    if (result2 < -15.0):
+        return str(i)
+    else:
+        return ''
+
+
+files = os.listdir('G:/Проекты/Fintech.Inview/images')
+
+otvet = ''
+for i in range(0,len(files)):
+    img_url = 'G:/Проекты/Fintech.Inview/images/' + str(i)+'.jpg'
+    result = CF.face.detect(img_url, face_id=False, landmarks=False, attributes="headPose")
+    print(result)
+    switch_case = {
+        'turn left': turn_left(result, i),
+        'turn right': turn_right(result, i),
+        'roll left': roll_left(result, i),
+        'roll right': roll_right(result, i)
+    }
+    if(switch_case[position] !=''):
+        otvet=otvet+switch_case[position]+' '
+    print(otvet)
+
+print(otvet)
